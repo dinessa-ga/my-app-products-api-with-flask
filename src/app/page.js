03fe7d83922ce3d6028b25273cf41cf63d71e0eb
dinessa-ga <dinessa.ga@gmail.com>
2023-10-22 11:57:29 -0500

    feat: cambios en page.js para la tabla de productos

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,49 @@
+import React from 'react';
+import { Button } from "@mui/material";
+
+
+
+
 
 
 export default function Home() {
   return (
     <>
-    <h1>APP</h1>
+    <h1 align="center">Lista de productos</h1>
+    <Button variant="contained">Mostrar</Button>
+    <div align="center">
+                <input
+                    type="text"
+                    placeholder="Filtrar por Código"
+                    // value={filterCode}
+                    // onChange={(e) => setFilterCode(e.target.value)}
+                />
+                <input
+                    type="date"
+                    // value={filterDate}
+                    // onChange={(e) => setFilterDate(e.target.value)}
+                />
+            </div>
+    <table align="center">
+       <thead>
+            <tr>
+                <th>Código</th>
+                <th>Descripción</th>
+                <th>Estado</th>
+                <th>Fecha de Vencimiento</th>
+                <th>Acciones</th>
+            </tr>
+       </thead>
+            <tr>
+              <td>01</td>
+              <td>laptop</td>
+              <td>disponible</td>
+              <td>01/11/23</td>
+              <td>buton</td>
+            </tr>
+        
+    </table>
+    
     </>
   )
 }
--- src/index.js
+++ src/index.js
@@ -4,3 +4,16 @@ import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
 // colocar antes del index por si requiere hacer algunos estilos globales
+
+import * as React from 'react';
+import * as ReactDOM from 'react-dom/client';
+import { StyledEngineProvider } from '@mui/material/styles';
+import Demo from './Demo';
+
+ReactDOM.createRoot(document.querySelector("#root")).render(
+  <React.StrictMode>
+    <StyledEngineProvider injectFirst>
+      <Demo />
+    </StyledEngineProvider>
+  </React.StrictMode>
+);
