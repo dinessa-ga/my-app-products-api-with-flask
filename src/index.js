345a11fbccfb9538c512c17f00829d304f135917
dinessa-ga <dinessa.ga@gmail.com>
2023-10-21 23:59:46 -0500

    dependencies: npm install para material UI

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+// colocar antes del index por si requiere hacer algunos estilos globales
